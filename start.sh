906db014c23b1f59e3f9196396e64509d63cc02d
Subjekt_91 <subjekt_91@gmx.de>
2017-08-03 00:52:56 +0200

    Changes to be committed: 	modified:   start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #High Charity Servers By Subjekt_91
 
-java -Xmx3g -Xms1024M -XX:ParallelGCThreads=2 -d64 -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -Dfml.debugExit=true -jar lurch-craft-server.jar nogui
+java -Xmx3g -XX:ParallelGCThreads=2 -d64 -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -Dfml.debugExit=true -jar lurch-craft-server.jar nogui
 
