5d0662df4d54f672f2934c000eb4f124a468ad35
Subjekt_91 <subjekt_91@gmx.de>
2017-09-09 21:43:18 +0200

    Start parameter an gepast (ParallelGCThreads)
    ParallelGCThreads von 2 auf 4 gesetzt

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #High Charity Servers By Subjekt_91
 
-java -server -Xmx10g -XX:ParallelGCThreads=2 -d64 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -Dfml.debugExit=true -jar lurch-craft-server.jar nogui
+java -server -Xmx10g -XX:ParallelGCThreads=4 -d64 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -Dfml.debugExit=true -jar lurch-craft-server.jar nogui
 
