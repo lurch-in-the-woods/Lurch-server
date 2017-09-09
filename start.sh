b999117aae8cdea8f27241d71a489ff6ba7ac78f
Subjekt_91 <subjekt_91@gmx.de>
2017-09-09 19:55:17 +0200

    Start Parameter erweitert
    den MarkSweepGC in den para metern geaddet
    und auf 10 GB max ram erhöt

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 #High Charity Servers By Subjekt_91
 
-java -server -Xmx4g -XX:ParallelGCThreads=2 -d64 -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -Dfml.debugExit=true -jar lurch-craft-server.jar nogui
 
